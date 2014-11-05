ff49873d85a1489391a2c06e0c1800933d504de8
Manfred Hantschel <thred@users.noreply.github.com>
2014-11-05 10:19:10 +0100

    koe another sample webapp (during w-jax talk)

---
+++ samples/spring-boot-demo/src/main/java/org/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package org.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class DemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping
+	public String echo(@RequestParam("text") String txt) {
+		return txt;
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+}
