8dc19c6842fee6d6d50295bb62831fa0cccd0352
derkoe <christian.koeberl@gmail.com>
2015-10-11 11:29:37 +0200

    Added websocket sample, refs #14

---
+++ samples/websockets/src/main/java/demo/Greeting.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ samples/websockets/src/main/java/demo/HelloMessage.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class HelloMessage {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+}
