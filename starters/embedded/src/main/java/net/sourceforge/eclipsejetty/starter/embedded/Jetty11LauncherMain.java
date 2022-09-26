21d1396a2572cc2e288a05c28478fc8a43c43c84
Christian Köberl <christian.koeberl@porscheinformatik.com>
2022-09-26 13:51:26 +0200

    Bump versions

--- starters/embedded/src/main/java/net/sourceforge/eclipsejetty/starter/embedded/Jetty11LauncherMain.java
+++ starters/embedded/src/main/java/net/sourceforge/eclipsejetty/starter/embedded/Jetty11LauncherMain.java
@@ -9,7 +9,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package net.sourceforge.eclipsejetty.starter.jetty9;
+package net.sourceforge.eclipsejetty.starter.embedded;
 
 import java.io.File;
 import java.io.PrintStream;
--- starters/embedded/src/main/java/net/sourceforge/eclipsejetty/starter/embedded/command/OpenCommand.java
+++ starters/embedded/src/main/java/net/sourceforge/eclipsejetty/starter/embedded/command/OpenCommand.java
@@ -9,7 +9,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package net.sourceforge.eclipsejetty.starter.jetty9.command;
+package net.sourceforge.eclipsejetty.starter.embedded.command;
 
 import java.awt.Desktop;
 import java.net.URI;
